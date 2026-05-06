e12e7cfd37c52621d513cbce85bdad74b6778085
DEVANSH BANSAL <153676380+DEVANSHBANSAL789@users.noreply.github.com>
2026-05-06 11:50:55 +0530

    Update vite.config.js

--- task-manager/frontend/vite.config.js
+++ task-manager/frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This keeps it open for Railway
+    allowedHosts: [
+      'task-manager-production-5622.up.railway.app'
+    ]
+  }
 })
